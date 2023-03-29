b7226949763675e84536845c014531bc795037d9
juanequex <ramon.g.uz37@gmail.com>
2023-03-28 19:13:44 -0600

    Custom cards with content

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -2,7 +2,15 @@ const List = ({ users }: any) => {
   console.log(users)
   return (
     <div>
-      Users list
+      {
+        users?.map((user: any, index: number) => 
+          <div key={`${user.id}-${index}`} className="card">
+            <h3>{user.name}</h3>
+            <p>Email: {user.email}</p>
+            <p>Website: {user.website}</p>
+          </div>
+        )
+      }
     </div>
   )
 }
