6d254676b06a5e1bd94f8d67158c295c0af4dbba
juanequex <ramon.g.uz37@gmail.com>
2023-03-28 18:30:08 -0600

    My first SSG api consume

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,4 +1,26 @@
-const Lista = () => <div>Hola Soy La nueva ruta de listas</div>
+const List = ({ users }: any) => {
+  console.log(users)
+  return (
+    <div>
+      Users list
+    </div>
+  )
+}
 
+export async function getStaticProps() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const result = await response.json();
+    console.log(result)
+    return {
+      props: {
+        users: result
+      },
+    }
+  } catch (error) {
+    console.log(error)
+    return {}
+  }
+}
 
-export default Lista;
+export default List;
